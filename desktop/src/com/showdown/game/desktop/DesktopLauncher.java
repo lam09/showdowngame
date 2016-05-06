73bf72ae2b9bd11d125920650c7d27a31fa59dc6
a.lam.tuan <a.lam.tuan@agsoft.sk>
2016-05-06 08:49:55 +0200

    first commit

---
+++ desktop/src/com/showdown/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.showdown.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.showdown.game.ShowdownCard;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShowdownCard(), config);
+	}
+}
