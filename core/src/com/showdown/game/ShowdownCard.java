af59832eeaaf7f4faaadfd5c615adb42f26c188d
a.lam.tuan <a.lam.tuan@agsoft.sk>
2016-05-06 14:48:58 +0200

    assets

--- core/src/com/showdown/game/ShowdownCard.java
+++ core/src/com/showdown/game/ShowdownCard.java
@@ -6,11 +6,18 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.showdown.game.states.GameStateManager;
 
 public class ShowdownCard extends ApplicationAdapter {
+	public static final int WIDTH= 150;
+	public static final int HEIGHT= 800;
+
+	GameStateManager gsm;
+
 	SpriteBatch batch;
 	Texture img;
 	Music music;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -21,10 +28,10 @@ public class ShowdownCard extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+//		batch.draw(img, 0, 0);
 		batch.end();
 	}
 }
